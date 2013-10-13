38cd11705a6b22514c5dcfebd3c0a25e18282738
ahawkins <adam@hawkins.io>
2013-10-13 20:05:19 +0200

    Group rack enhancements

--- lib/chassis.rb
+++ lib/chassis.rb
@@ -5,7 +5,6 @@ require 'sinatra'
 require 'manifold'
 require 'rack/contrib/bounce_favicon'
 require 'rack/contrib/post_body_content_type_parser'
-require 'rack/builder_shim_patch'
 
 require 'harness'
 require 'harness/rack'
@@ -25,13 +24,14 @@ end
 require_relative 'chassis/form'
 require_relative 'chassis/repo'
 
-require_relative 'chassis/request_logger'
-require_relative 'chassis/no_robots'
-require_relative 'chassis/status_check'
-
 require_relative 'chassis/hash_initializer'
 
 require_relative 'chassis/dirty_tracking'
 require_relative 'chassis/observable'
+
+require_relative 'chassis/rack/request_logger'
+require_relative 'chassis/rack/no_robots'
+require_relative 'chassis/rack/status_check'
+require_relative 'chassis/rack/builder_shim_patch'
 
 require_relative 'chassis/web_app'
