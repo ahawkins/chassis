59fbf77d10e6967bb5b680228a168494cb59f32f
ahawkins <adam@hawkins.io>
2013-10-30 00:59:23 +0200

    Allow repos to use custom backends

--- lib/chassis/repo/delegation.rb
+++ lib/chassis/repo/delegation.rb
@@ -2,51 +2,55 @@ module Chassis
   class Repo
     module Delegation
       def all
-        Repo.instance.all object_class
+        backend.all object_class
       end
 
       def count
-        Repo.instance.count object_class
+        backend.count object_class
       end
 
       def find(id)
-        Repo.instance.find object_class, id
+        backend.find object_class, id
       end
 
       def save(record)
-        Repo.instance.save(record)
+        backend.save(record)
       end
 
       def delete(record)
-        Repo.instance.delete record
+        backend.delete record
       end
 
       def first
-        Repo.instance.first object_class
+        backend.first object_class
       end
 
       def last
-        Repo.instance.last object_class
+        backend.last object_class
       end
 
       def query(selector)
-        Repo.instance.query object_class, selector
+        backend.query object_class, selector
       end
 
       def sample
-        Repo.instance.sample object_class
+        backend.sample object_class
       end
 
       def graph(id)
-        Repo.instance.graph object_class, id
+        backend.graph object_class, id
       end
 
       def graph_query(selector)
-        Repo.instance.graph_query object_class, selector
+        backend.graph_query object_class, selector
       end
 
       def object_class
         @object_class ||= self.to_s.match(/^(.+)Repo/)[1].constantize
+      end
+
+      def backend
+        Repo.instance
       end
     end
   end
