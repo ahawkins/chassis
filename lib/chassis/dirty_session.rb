bd047435238a1160e206b04507f1366c4e0ac412
ahawkins <adam@hawkins.io>
2014-02-02 14:47:26 +0100

    Create Chassis::DirtySession

---
+++ lib/chassis/dirty_session.rb
@@ -0,0 +1,82 @@
+module Chassis
+  class DirtySession < Proxy
+    def initialize(*args)
+      super
+      @original_values  = { }
+      @new_values = { }
+    end
+
+    def clean?
+      new_values.empty?
+    end
+
+    def dirty?
+      !clean?
+    end
+
+    def original_values
+      @original_values
+    end
+
+    def new_values
+      @new_values
+    end
+
+    def method_missing(name, *args, &block)
+      raise MissingObject, name unless __getobj__
+
+      if writer_method?(name)
+        handle_writer_method name, *args, &block
+      elsif changed_method?(name)
+        handle_changed_method name
+      elsif original_method?(name)
+        handle_original_method name
+      else
+        __getobj__.send name, *args, &block
+      end
+    end
+
+    private
+    def handle_writer_method(name, *args, &block)
+      original_value = __getobj__.send reader_method(name)
+      original_values[reader_method(name)] = original_value
+
+      new_value = __getobj__.send name, *args, &block
+      new_values[reader_method(name)] = new_value
+
+      new_value
+    end
+
+    def writer_method?(name)
+      name =~ /=$/
+    end
+
+    def reader_method(name)
+      method_name = name.to_s
+
+      if writer_method? method_name
+        method_name.match(/(.+)=$/)[1].to_sym
+      elsif changed_method? method_name
+        method_name.match(/(.+)_changed\?$/)[1].to_sym
+      elsif original_method? method_name
+        method_name.match(/original_(.+)$/)[1].to_sym
+      end
+    end
+
+    def original_method?(name)
+      name =~ /original_.+$/
+    end
+
+    def changed_method?(name)
+      name =~ /_changed\?$/
+    end
+
+    def handle_changed_method(name)
+      original_values.key? reader_method(name)
+    end
+
+    def handle_original_method(name)
+      original_values[reader_method(name)]
+    end
+  end
+end
---
+++ test/dirty_session_test.rb
@@ -0,0 +1,79 @@
+require_relative 'test_helper'
+
+class DirtySessionTest < MiniTest::Unit::TestCase
+  class Person
+    include Chassis::Initializable
+
+    attr_accessor :name, :email, :id
+  end
+
+  attr_reader :person
+
+  def setup
+    @person = Person.new do |person|
+      person.name = 'adam'
+      person.email = 'example@example.com'
+      person.id = 1
+    end
+  end
+
+  def test_initialize_as_clean
+    session = Chassis::DirtySession.new person
+    assert session.clean?
+    refute session.dirty?
+  end
+
+  def test_assigning_new_value_marks_the_session_dirty
+    session = Chassis::DirtySession.new person
+    session.name = 'foo'
+
+    refute session.clean?
+    assert session.dirty?
+  end
+
+  def test_changes_can_be_tested_via_query_method
+    session = Chassis::DirtySession.new person
+    session.name = 'foo'
+
+    refute session.clean?
+    assert session.dirty?
+
+    assert session.name_changed?
+  end
+
+  def test_changes_are_accessible
+    session = Chassis::DirtySession.new person
+    session.name = 'foo'
+
+    refute session.clean?
+    assert session.dirty?
+
+    assert_equal({ name: 'foo' }, session.new_values)
+  end
+
+  def test_assigning_a_new_value_records_the_original_value
+    original_name = person.name
+    assert original_name, "Precondition: person must have a name"
+
+    session = Chassis::DirtySession.new person
+    session.name = 'foo'
+
+    refute session.clean?
+    assert session.dirty?
+
+    assert_equal({ name: original_name}, session.original_values)
+  end
+
+  def test_original_values_are_accessible_via_an_original_method
+    original_name = person.name
+    assert original_name, "Precondition: person must have a name"
+
+    session = Chassis::DirtySession.new person
+    session.name = 'foo'
+
+    refute session.clean?
+    assert session.dirty?
+
+    assert_equal original_name, session.original_name
+  end
+end
