fa50891486dfcd68199cc3a889b047a28ff1b490
ahawkins <adam@hawkins.io>
2014-03-06 23:54:47 +0100

    Add Chassis.delegate

---
+++ test/delegate_test.rb
@@ -0,0 +1,33 @@
+require_relative 'test_helper'
+
+class DelegateTest < MiniTest::Unit::TestCase
+  class Delegator
+    include Chassis.delegate(:add, to: :object)
+
+    attr_reader :object
+
+    def initialize(object)
+      @object = object
+    end
+  end
+
+  def test_methods_are_delegated
+    delegate = Class.new do
+      def add(number)
+        number + 5
+      end
+    end.new
+
+    delegator = Delegator.new delegate
+
+    assert_equal 10, delegator.add(5)
+  end
+
+  def test_raises_an_error_if_no_object_to_delegate_to
+    delegator = Delegator.new nil
+
+    assert_raises Chassis::DelegationError do
+      delegator.add 1
+    end
+  end
+end
